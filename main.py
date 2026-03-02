e5e8eb89c99e67c63972ca569c5abfd62d315970
Gamal <gamalalmaqtary6838@gmail.com>
2026-03-02 13:09:28 +0300

    h

--- main.py
+++ main.py
@@ -99,4 +99,4 @@ async def get_file(filename: str):
 if __name__ == "__main__":
     import uvicorn
     print("تشغيل FastAPI على http://127.0.0.1:8000/")
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", port=8000, reload=False)
