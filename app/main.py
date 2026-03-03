f80f46e0f7a64e2887142ae7bf25f322a6bffed3
Gamal <gamalalmaqtary6838@gmail.com>
2026-03-03 20:32:45 +0300

    h

--- app/main.py
+++ app/main.py
@@ -24,9 +24,9 @@ app = FastAPI()
 # STATIC / TEMPLATES
 # =========================
 
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-templates = Jinja2Templates(directory="app/templates")
+templates = Jinja2Templates(directory="templates")
 
 
 # =========================
